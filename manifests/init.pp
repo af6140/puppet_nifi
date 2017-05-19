36157f7b188c0b67f0843ba8b9abaf9c329d6efc
Wang, Dawei <dwang@entertainment.com>
2017-05-19 15:40:34 -0400

    validate class paramenters

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,47 +11,32 @@
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class nifi (
-  $package_name = $::nifi::params::package_name,
+  String[1] $package_name = $::nifi::params::package_name,
   $service_name = $::nifi::params::service_name,
   $package_version = $::nifi::params::package_version,
   $nifi_conf_dir = $::nifi::params::nifi_conf_dir,
-  $nifi_properties = {},
+  Optional[Hash[String[1],String[1]]] $nifi_properties = {},
   $min_heap = '512m',
   $max_heap = '512m',
-  $config_cluster = false,
-  $cluster_members = [],
-  $cluster_identities=[],
-  $ldap_provider_configs = {},
-  $ldap_id_mappings = undef,
-  $config_ssl=false,
-  $initial_admin_identity = undef,
+  Boolean $config_cluster = false,
+  Optional[Array[String[1]]] $cluster_members = [],
+  Optional[Array[String[1]]] $cluster_identities=[],
+  Hash[String[1],String[1]] $ldap_provider_configs = {},
+  Optional[Array[Struct[{pattern => String[1], value=>String[1], ensure => Optional[Enum[present,absent]], index => Optional[Integer[0,9]] }],0,9]] $id_mappings = undef,
+  Boolean $config_ssl=false,
+  Optional[String[1]] $initial_admin_identity = undef,
   $cacert_path = undef,
   $node_cert_path = undef,
   $node_private_key_path = undef,
   $initial_admin_cert_path = undef,
   $initial_admin_key_path = undef,
-  $keystore_password = 'changeit',
-  $key_password = undef,
-  $client_auth = false,
+  String[6] $keystore_password = 'changeit',
+  Optional[String[6]] $key_password = undef,
+  Boolean $client_auth = false,
   $custom_properties_file = $::nifi::params::custom_properties_file,
-  $custom_properties = undef,
+  Optional[Hash[String[1],String[1]]] $custom_properties = undef,
 ) inherits ::nifi::params {
 
-  # validate parameters here
-
-  assert_type(String[1], $package_name)
-  assert_type(String[1], $package_version)
-
-  assert_type(Boolean, $config_cluster)
-  assert_type(Boolean, $config_ssl)
-
-  if $cluster_members {
-    assert_type(Array, $cluster_members)
-  }
-
-  if $ldap_id_mappings {
-    assert_type(Array[Hash[String, String], 0, 99], $ldap_id_mappings)
-  }
 
   if($config_cluster) {
     $count_of_nodes = size($cluster_members)

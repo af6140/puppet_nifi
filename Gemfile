b8db7b75f0b02e5d6674afa2ebb0575dbf636d94
af6140 <daweiwang.gatekeeper@gmail.com>
2017-07-13 14:52:57 -0400

    permission provider

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :test do
   gem "puppet-lint-version_comparison-check"
   gem "puppet-lint-classes_and_types_beginning_with_digits-check"
   gem "puppet-lint-unquoted_string-check"
+  gem "webmock"
 end
 
 group :development do
---
+++ lib/puppet_x/ent/nifi/rest.rb
@@ -0,0 +1,80 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'ent', 'nifi', 'config.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'ent', 'nifi', 'exception.rb'))
+require 'json'
+require 'rest-client'
+module Ent
+  module Nifi
+    class Rest
+      def self.request
+        nifi_config = Ent::Nifi::Config.config
+          nifi = RestClient::Resource.new(
+            nifi_config[:url],
+            :ssl_client_cert => nifi_config[:cert],
+            :ssl_client_key => nifi_config[:key],
+            :verify_ssl => OpenSSL::SSL::VERIFY_NONE
+          )
+        yield nifi
+      end
+
+      def self.get_all(resource_name)
+        puts "Get all *************** #{resource_name}"
+        request { |nifi|
+          begin
+            response = nifi[resource_name].get(:accept => :json)
+          rescue => e
+            Ent::Nifi::ExceptionHandler.process(e) { |msg|
+              raise "Could not request #{resource_name} from #{nifi.url}: #{msg}"
+            }
+          end
+
+          begin
+            JSON.parse(response)
+          rescue => e
+            raise "Could not parse the JSON response from Nexus (url: #{nifi.url}, resource: #{resource_name}): #{e} (response: #{response})"
+          end
+        }
+      end
+
+      def self.create(resource_name, data)
+        request { |nifi|
+          begin
+            puts "#{data}"
+            nifi[resource_name].post JSON.generate(data), :accept => :json, :content_type => :json
+          rescue => e
+            Ent::Nifi::ExceptionHandler.process(e) { |msg|
+              raise "Could not create #{resource_name} at #{nifi.url}: #{msg}"
+            }
+          end
+        }
+      end
+
+      def self.update(resource_name, data)
+        request { |nifi|
+          begin
+            nifi[resource_name].put JSON.generate(data), :accept => :json, :content_type => :json
+          rescue => e
+            Ent::Nifi::ExceptionHandler.process(e) { |msg|
+              raise "Could not update #{resource_name} at #{nifi.url}: #{msg}"
+            }
+          end
+        }
+      end
+
+      def self.destroy(resource_name)
+        service.ensure_running
+        request { |nifi|
+          begin
+            nifi[resource_name].delete :accept => :json
+          rescue RestClient::ResourceNotFound
+            # resource already deleted, nothing to do
+          rescue => e
+            Ent::Nifi::ExceptionHandler.process(e) { |msg|
+              raise "Could not delete #{resource_name} at #{nifi.url}: #{msg}"
+            }
+          end
+        }
+      end
+
+    end
+  end
+end

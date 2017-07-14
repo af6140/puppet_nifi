372ee8c590cb12a4db0f732bd671496da70a5fef
Wang, Dawei <dwang@entertainment.com>
2017-07-14 14:39:15 -0400

    refactor

--- lib/puppet/provider/nifi_group/ruby.rb
+++ lib/puppet/provider/nifi_group/ruby.rb
@@ -1,54 +1,44 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'nifi'))
-
-Puppet::Type.type(:nifi_user).provide(:curl, :parent=> Puppet::Provider::Nifi ) do
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'ent', 'nifi', 'rest.rb'))
+Puppet::Type.type(:nifi_group).provide(:ruby, :parent=> Puppet::Provider::Nifi ) do
 
   mk_resource_methods
 
-  def exists?
-      existing_user = get_user(@resource[:name])
-      ! existing_user.nil?
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
   end
 
-  def get_user(identity)
-    #ver=java('-version')
-    #puts("java version: #{ver}")
-    #curl -X GET api_url/tenants/search-result?q=user -H 'cache-control: no-cache'
-    # example response
-    #{
-    # "users": [
-    #   {
-    #     "revision": {
-    #   "version": 0
-    # },
-    #   "id": "65a6839c-015c-1000-ffff-ffffee0e468c",
-    #   "permissions": {
-    #   "canRead": true,
-    # "canWrite": true
-    # },
-    #   "component": {
-    #   "id": "65a6839c-015c-1000-ffff-ffffee0e468c",
-    #   "identity": "username"
-    # }
-    # }
-    # ],
-    #   "userGroups": []
-    # }
-    #
-
-    search_command = ['-k', '-s', '-X', 'GET', '--cert', cert_path, '--key', key_path, "#{api_url}/tenants/search-results?q=#{@resource[:name]}"]
-    puts("search command #{search_command}")
-    search_response = curl(search_command)
-    #puts "search response = #{search_response}"
-    response_json = JSON.parse(search_response)
-    found_user = response_json['users'].select do | user |
-      #puts user.to_json
-      user['component']['identity'] == identity
+  def self.instances
+    search_json = Ent::Nifi::Rest.get_all("tenants/user-groups")
+    if search_json.nil?
+      return []
     end
-    #puts("found user #{found_user}")
-    found_user[0]
+    groups_raw = search_json['userGroups']
+    groups_raw.map do |group_json|
+      new(:name => group_json['component']['identity'],
+          :ensure => :present
+      )
+    end
   end
 
-  def create_user
+  def self.prefetch(resources)
+    # resources parameter is an hash of resources managed by Puppet (catalog)
+    resource_instances = instances
+    resources.keys.each do |name|
+      if provider = resource_instances.find{ |r| r.name == name }
+        r[name].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+
+
+  def create_group
     #Example post
     # {
     #   "revision": {
@@ -105,22 +95,22 @@ Puppet::Type.type(:nifi_user).provide(:curl, :parent=> Puppet::Provider::Nifi ) 
           "parentGroupId": null,
           "identity": "#{username}",
           "userGroups": [],
+          "users": [],
           "accessPolicies": []
         }
       }
     }
-    #puts "request_json :#{req_json}"
-    curl(['-k', '-s', '-X', 'POST', '--cert', cert_path, '--key', key_path, "#{api_url}/tenants/users"])
+    curl(['-k', '-s', '-X', 'POST', '--cert', cert_path, '--key', key_path, "#{api_url}/tenants/user-groups"])
   end
 
 
-  def delete_user
-    exisiting_user = get_user(@resource[:name])
-    if ! exisiting_user.nil?
-      user_id = exisiting_user['id']
-      delete_request_url= "#{self.api_url}/tenants/users/#{user_id}"
+  def delete_group
+    exisiting_group = get_group(@resource[:name])
+    if ! exisiting_group.nil?
+      user_id = exisiting_group['id']
+      delete_request_url= "#{api_url}/tenants/user-groups/#{user_id}"
       delete_response= curl(['-k', '-s', '-X', 'DELETE', '--cert', cert_path, '--key', key_path, delete_request_url])
-      #puts "Delete user response: #{delete_response}"
+      #puts "Delete gruop response: #{delete_response}"
       if delete_response
         response_json = JSON.parse(delete_response)
         response_code = response_json['status']
@@ -130,16 +120,26 @@ Puppet::Type.type(:nifi_user).provide(:curl, :parent=> Puppet::Provider::Nifi ) 
   end
 
   def create
-    create_user
+    create_group
     @property_hash[:ensure] = :present
     exists? ? (return true) : (return false)
   end
 
   def destroy
-    delete_user
-    @property_hash.clear
-    still_there = exists?
-    still_there ? (return false) :(return true)
+
+  end
+
+  def refresh
+    # ...
+  end
+
+  # Called once after Puppet creates, destroys or updates a resource
+  def flush
+    if @property_flush
+      # update resource here
+      @property_flush = nil
+    end
+    @property_hash = resource.to_hash
   end
 
 end

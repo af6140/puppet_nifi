971d5f9f48029e7a1da727bc081c41da36529fa8
af6140 <daweiwang.gatekeeper@gmail.com>
2017-07-21 10:30:37 -0400

    configurable cluster node group name

--- manifests/cluster_policy.pp
+++ manifests/cluster_policy.pp
@@ -1,36 +1,37 @@
 class nifi::cluster_policy(
-  $cluster_members = $::nifi::cluster_members
+  $cluster_members = $::nifi::cluster_members,
+  $cluster_group = 'cluster_nodes'
 ){
   #create cluster node permissions
-  nifi_group {'cluster_nodes':
+  nifi_group {$cluster_group:
     ensure => 'present'
   }
-  nifi_permission {"controller:read:group:cluster_nodes":
+  nifi_permission {"controller:read:group:${cluster_group}":
     ensure => 'present'
   }
-  nifi_permission {"proxy:write:group:cluster_nodes":
+  nifi_permission {"proxy:write:group:${cluster_group}":
     ensure => 'present'
   }
-  nifi_permission {"site-to-site:read:group:cluster_nodes":
+  nifi_permission {"site-to-site:read:group:${cluster_group}":
     ensure => 'present'
   }
-  nifi_permission {"provenance:read:group:cluster_nodes":
+  nifi_permission {"provenance:read:group:${cluster_group}":
     ensure => 'present'
   }
   $root_pg = parsejson($::nifi_root_process_group)
   notify{"${root_pg}": }
   if $root_pg {
     $root_pg_id = $root_pg['id']
-    nifi_permission { "data/process-groups/${root_pg_id}:read:group:cluster_nodes":
+    nifi_permission { "data/process-groups/${root_pg_id}:read:group:${cluster_group}":
       ensure => 'present'
     }
-    nifi_permission { "data/process-groups/${root_pg_id}:write:group:cluster_nodes":
+    nifi_permission { "data/process-groups/${root_pg_id}:write:group:${cluster_group}":
       ensure => 'present'
     }
-    nifi_permission { "process-groups/${root_pg_id}:read:group:cluster_nodes":
+    nifi_permission { "process-groups/${root_pg_id}:read:group:${cluster_group}":
       ensure => 'present'
     }
-    nifi_permission { "process-groups/${root_pg_id}:write:group:cluster_nodes":
+    nifi_permission { "process-groups/${root_pg_id}:write:group:${cluster_group}":
       ensure => 'present'
     }
 

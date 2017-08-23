c7f2576b2587b65aa0e48d72e233ab918e0dfd3e
af6140 <daweiwang.gatekeeper@gmail.com>
2017-08-23 15:06:57 -0400

    more checks

--- manifests/admin_policy.pp
+++ manifests/admin_policy.pp
@@ -43,23 +43,24 @@ class nifi::admin_policy(
     ensure => 'present'
   }
 
-  $root_pg = parsejson($::nifi_root_process_group, undef)
-  if $root_pg {
-    $root_pg_id = $root_pg['id']
-    nifi_permission {"data/process-groups/${root_pg_id}:read:group:${admin_group}":
-      ensure => 'present'
-    }
-    nifi_permission {"data/process-groups/${root_pg_id}:write:group:${admin_group}":
-      ensure => 'present'
-    }
+  if $::nifi_root_process_group {
+    $root_pg = parsejson($::nifi_root_process_group)
+    if $root_pg {
+      $root_pg_id = $root_pg['id']
+      nifi_permission { "data/process-groups/${root_pg_id}:read:group:${admin_group}":
+        ensure => 'present'
+      }
+      nifi_permission { "data/process-groups/${root_pg_id}:write:group:${admin_group}":
+        ensure => 'present'
+      }
 
-    nifi_permission {"process-groups/${root_pg_id}:read:group:${admin_group}":
-      ensure => 'present'
-    }
-    nifi_permission {"process-groups/${root_pg_id}:write:group:${admin_group}":
-      ensure => 'present'
-    }
+      nifi_permission { "process-groups/${root_pg_id}:read:group:${admin_group}":
+        ensure => 'present'
+      }
+      nifi_permission { "process-groups/${root_pg_id}:write:group:${admin_group}":
+        ensure => 'present'
+      }
 
+    }
   }
-
 }
